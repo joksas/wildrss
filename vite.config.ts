432837e46f3cdfc5e194c9e0f5f791ac31a3cfa5
Dovydas Joksas <hello@dovydas.com>
2025-11-15 22:43:31 +0000

    Remove Devtools On Build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
-import { devtools } from "@tanstack/devtools-vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
-import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import viteTsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    devtools({ removeDevtoolsOnBuild: false }),
+    devtools({ removeDevtoolsOnBuild: true }),
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     viteTsConfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
